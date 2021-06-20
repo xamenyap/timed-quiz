8156e4b6cce0ff44d962a94db349bc7d9e59f68d
Xamenyap <jpham2811@gmail.com>
2021-06-20 16:04:22 +0700

    Shorten problems.csv, read the file and compare the answers to get correct/total ratio

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	f, err := os.Open("problems.csv")
+	if err != nil {
+		log.Fatal("unable to open file", err)
+	}
+
+	r := csv.NewReader(f)
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal("unable to read file", err)
+	}
+
+	sc := bufio.NewScanner(os.Stdin)
+	var (
+		correct int
+		total   int
+	)
+
+	for _, rec := range records {
+		fmt.Println(rec[0] + "?")
+		if !sc.Scan() {
+			log.Fatal("unable to read stdin")
+		}
+
+		if sc.Text() == rec[1] {
+			correct++
+		}
+
+		total++
+	}
+
+	fmt.Printf("You answer %d/%d correctly", correct, total)
 }
